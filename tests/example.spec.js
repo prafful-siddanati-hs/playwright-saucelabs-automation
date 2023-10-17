3ce48a82893f2c0ba717d9ef59fdffa1f261e8d2
lavanya petchetti <lavanya.petchetti@hootsuite.com>
2023-10-16 17:07:37 -0700

    Fix SN picker

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -28,7 +28,7 @@ test('drag and drop card on week view', async ({ page }) => {
   await page.getByLabel('Post').click();
   await page.getByPlaceholder('Select a social account').click();
   await page.getByTestId('MessageEditArea').getByText('HComposer1').first().click();
-  await page.getByText('Twitterhs_composer1@HComposer1• Just nowSocial networks regularly make updates t').click();
+  await page.locator('.vk-ComposerHeader').click();
 
   await page.getByLabel('Text').click();
   await page.getByLabel('Text').fill(composeText);
@@ -85,33 +85,33 @@ test('drag and drop media from side pane on week view', async ({ page }) => {
   await page.waitForTimeout(2000);
   await page.close();
 });
+
+// test('Bitly creation', async ({ page }) => {
+//   await page.goto('https://staging.hootsuite.com/login');
+//   await page.getByRole('textbox', { name: 'Please enter a valid email address' }).fill("prafful.siddanati+testaccount@hootsuite.com")
+//   await page.locator('#loginPasswordInput').fill("!1Password");
+//   await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+//   await page.waitForTimeout(2000);
 //
-// // test('Bitly creation', async ({ page }) => {
-// //   await page.goto('https://staging.hootsuite.com/login');
-// //   await page.getByRole('textbox', { name: 'Please enter a valid email address' }).fill("prafful.siddanati+testaccount@hootsuite.com")
-// //   await page.locator('#loginPasswordInput').fill("!1Password");
-// //   await page.getByRole('button', { name: 'Sign in', exact: true }).click();
-// //   await page.waitForTimeout(2000);
-// //
-// //   await page.locator('[data-test-id="global-nav-account-button"]').click();
-// //
-// //   await page.locator('[data-test-id="Manage accounts and teams"]').click();
-// //   await page.getByRole('button', { name: 'Link Settings' }).nth(1).click();
-// //   await page.getByRole('button', { name: 'Bit.ly shorteners' }).click();
-// //   await page.getByRole('button', { name: 'Create new Bit.ly shortener' }).click();
-// //   await page.getByPlaceholder('Shortener name...').click();
-// //   await page.getByPlaceholder('Shortener name...').fill(`test bitly ${Date.now()}`);
-// //   const page1Promise = page.waitForEvent('popup');
-// //   await page.getByRole('button', { name: 'Add new Bit.ly shortener' }).click();
-// //   const page1 = await page1Promise;
-// //   await page1.getByRole('button', { name: 'Close' }).click();
-// //   await page1.getByRole('link', { name: 'Sign in with your Bitly account' }).click();
-// //   await page1.locator('#sign-in input[name="username"]').click();
-// //   await page1.locator('#sign-in input[name="username"]').fill('PraffulS');
-// //   await page1.locator('#sign-in input[name="password"]').click();
-// //   await page1.locator('#sign-in input[name="password"]').fill('tKq54RWaw362');
-// //   await page1.getByRole('button', { name: 'Log in' }).click();
-// //   await page1.getByRole('button', { name: 'Allow' }).click();
-// //   await page1.close();
-// //   await page.close();
-// // });
+//   await page.locator('[data-test-id="global-nav-account-button"]').click();
+//
+//   await page.locator('[data-test-id="Manage accounts and teams"]').click();
+//   await page.getByRole('button', { name: 'Link Settings' }).nth(1).click();
+//   await page.getByRole('button', { name: 'Bit.ly shorteners' }).click();
+//   await page.getByRole('button', { name: 'Create new Bit.ly shortener' }).click();
+//   await page.getByPlaceholder('Shortener name...').click();
+//   await page.getByPlaceholder('Shortener name...').fill(`test bitly ${Date.now()}`);
+//   const page1Promise = page.waitForEvent('popup');
+//   await page.getByRole('button', { name: 'Add new Bit.ly shortener' }).click();
+//   const page1 = await page1Promise;
+//   await page1.getByRole('button', { name: 'Close' }).click();
+//   await page1.getByRole('link', { name: 'Sign in with your Bitly account' }).click();
+//   await page1.locator('#sign-in input[name="username"]').click();
+//   await page1.locator('#sign-in input[name="username"]').fill('PraffulS');
+//   await page1.locator('#sign-in input[name="password"]').click();
+//   await page1.locator('#sign-in input[name="password"]').fill('tKq54RWaw362');
+//   await page1.getByRole('button', { name: 'Log in' }).click();
+//   await page1.getByRole('button', { name: 'Allow' }).click();
+//   await page1.close();
+//   await page.close();
+// });
