0349e16ff720ed1e09c5e44e50f0358d18f40249
Prafful Siddanati <prafful.siddanati@hootsuite.com>
2023-10-17 14:44:07 -0700

    Update to use typescript

---
+++ playwright.config.ts
@@ -0,0 +1,60 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ * @see {https} ://playwright.dev/docs/test-configuration
+ * @param {string} key
+ * @param {string} defaultValue
+ */
+
+function getEnv (key: string, defaultValue: string): string {
+  let v = process.env[key] || '';
+  return v !== '' ? v : defaultValue;
+}
+
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [['html',{outputFile: 'tests.results.html', open: 'never'}]],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'https://www.staging.com/',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    timezoneId: 'America/Vancouver',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { 
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1920, height: 1080 },
+        saucelabs: {
+          username: getEnv('SAUCE_USERNAME', ''),
+          access_key: getEnv('SAUCE_ACCESS_KEY', ''),
+          runOnSaucelabs: true,
+          test_workers: {
+            enabled: true,
+            workers: 'auto'
+          }
+        }
+      },
+      
+    },
+  ],
+});
