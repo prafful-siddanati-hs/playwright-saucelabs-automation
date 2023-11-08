aa5986099a6804e656483c19695f63e0349383a9
lavanya petchetti <lavanya.petchetti@hootsuite.com>
2023-11-08 10:38:43 -0800

    updated sauce config with drafts suite

--- globals.js
+++ globals.js
@@ -5,7 +5,7 @@ module.exports = {
     tops_skyline: 'https://staging-api-auth.hootsuite.com/v1/organizations',
     broker_member_service: 'https://staging-api-auth.hootsuite.com/v1/members',
     service_message_publishing: 'https://staging-api-auth.hootsuite.com/publisher',
-    service_drafts: 'https://staging-api-auth.hootsuite.com/publisher',
+    service_drafts: 'https://staging-api-auth.hootsuite.com/publisher/drafts',
     service_trail: 'https://staging-api-auth.hootsuite.com/publisher',
     launch_url_cms: 'https://www-staging.hootsuite.com',
     login_url: 'https://staging.hootsuite.com/login?lang=en'
