ba11fb48905909f223995f36f6787fa925ef3d7f
Prafful Siddanati <prafful.siddanati@hootsuite.com>
2023-12-05 15:18:24 -0800

    Credential error checks

--- custom-commands/createUser.js
+++ custom-commands/createUser.js
@@ -51,6 +51,7 @@ class createUser extends events.EventEmitter{
         } catch (err) {
             console.assert(false, `Error creating user with billing. ${err}`);
         } finally {
+            console.log("Print to debug: ",member)
             //Update global storage with member details so that functions like getFixture() can access it.
             if (!global.member) {
                 global.member = []
