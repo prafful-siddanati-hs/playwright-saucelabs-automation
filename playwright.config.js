ef80b769f8f84cc99bc0cbf09cb807dfb81a9690
prafful.siddanati@hootsuite.com <prafful.siddanati@corp.hootsuitemedia.com>
2024-02-21 15:20:23 -0800

    Update safari resolution

--- playwright.config.js
+++ playwright.config.js
@@ -119,7 +119,7 @@ module.exports = defineConfig({
           },
           contextOptions: {
             ignoreHTTPSErrors: true,
-            viewport: { width: 1920, height: 1440 },
+            viewport: { width: 1920, height: 1080 },
           },
           video: 'on-first-retry'
         }
