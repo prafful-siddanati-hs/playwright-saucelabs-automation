a2c3bd03d42237136739b0f04af54e36f38c31b4
Lavanya.Petchetti <lavanya.petchetti@hootsuite.com>
2024-09-12 11:59:44 -0700

    Fixed mention test data

--- globals.js
+++ globals.js
@@ -157,7 +157,6 @@ module.exports = {
 				'MENT',
 				'The Good Judge-ment Podcast',
 				'Hoot',
-				'hoot',
 				'Music Business Worldwide (MBW)',
 				'Music Reports, Inc',
 				'Music Supervisor Inc.'
