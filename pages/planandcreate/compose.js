86afbca15c6c0de4078c0ed8d442746f53c981c9
Lavanya.Petchetti <lavanya.petchetti@hootsuite.com>
2024-10-18 16:23:48 -0700

    Added campaign picker test

--- pages/planandcreate/compose.js
+++ pages/planandcreate/compose.js
@@ -231,6 +231,10 @@ exports.ComposePage = class ComposePage {
 		this.emoticonStickers= page.locator('[data-testid="imageEditor"] button[data-test="Emoticons"]');
 		this.grinEmoticonSticker = page.locator('[data-testid="imageEditor"] [data-test="Grin"]');
 		this.imageEditorSaveButton = page.locator('#fe-app-image-editor button[aria-label="Save"]');
+		this.campaignPicker = page.locator('.vk-ComposerModal .vk-CampaignDropdownButton span');
+		this.campaignListBuuton = page.locator('.vk-ComposerModal  button.campaign-dropdown-button-item');
+		this.campaignSelected = page.locator('.vk-ComposerModal .rc-CampaignSelectDropdown span');
+		this.manageCampaignsButton = page.locator('.vk-ComposerModal .rc-CampaignSelectDropdown [role="listbox"] button', { hasText: 'Manage campaigns' });
 	}
 
 	async setDarkLaunchCookies() {
---
+++ tests/plan_and_create/composer/network_and_campaign_picker/campaign_picker.spec.js
@@ -0,0 +1,64 @@
+const { test, expect} = require('@playwright/test');
+const tearDown = require('../../../../custom-commands/tearDown');
+const getFixture = require('../../../../custom-commands/getFixture');
+const { LoginPage } = require('../../../../pages/login');
+const { ComposePage } = require('../../../../pages/planandcreate/compose');
+const { ManageCampaignsPage } = require('../../../../pages/planandcreate/manageCampaigns');
+let firstCampaign, secondCampaign;
+
+test.afterEach(async ({ page }) => {
+	const cleanUp = new tearDown();
+
+	await cleanUp.command();
+	await page.close();
+});
+
+test('Campaigns picker validation', async ({ page }) => {
+	const addFixture = new getFixture();
+	const loginPage = new LoginPage(page);
+	const composePage = new ComposePage(page);
+	const manageCampaignsPage = new ManageCampaignsPage(page);
+
+	await test.step('Setup user & accounts', async () => {
+		await addFixture.command('campaign_picker', 'linkedin_enterprise', true, 300);
+	});
+
+	await test.step('Login as pro user', async () => {
+		await loginPage.signIn('campaign_picker');
+	});
+
+	await test.step('Open composer', async () => {
+		await composePage.selectComposeButton();
+	});
+
+	await test.step('Select campaign picker and select campaign', async () => {
+		await expect(composePage.campaignPicker).toBeVisible();
+		await composePage.campaignPicker.click();
+		firstCampaign = await composePage.campaignListBuuton.first().innerText();
+		await expect(composePage.campaignListBuuton.first()).toBeVisible();
+		await composePage.campaignListBuuton.first().click();
+		await expect(composePage.campaignSelected).toHaveText(firstCampaign);
+	});
+
+	await test.step('Select another campaign from campaign picker', async () => {
+		await expect(composePage.campaignPicker).toBeVisible();
+		await composePage.campaignPicker.click();
+		secondCampaign = await composePage.campaignListBuuton.nth(1).innerText();
+		await expect(composePage.campaignListBuuton.nth(1)).toBeVisible();
+		await composePage.campaignListBuuton.nth(1).click();
+		await expect(composePage.campaignSelected).toHaveText(secondCampaign);
+	});
+
+	await test.step('Select manage campaign button from campaign picker', async () => {
+		await expect(composePage.campaignPicker).toBeVisible();
+		await composePage.campaignPicker.click();
+		await expect(composePage.manageCampaignsButton).toBeVisible();
+		await composePage.manageCampaignsButton.click();
+	});
+
+	await test.step('Verify manage campaign view', async () => {
+		await expect(manageCampaignsPage.manageCampaignsView).toBeVisible();
+		await expect(manageCampaignsPage.createCampaignButton).toBeVisible();
+	});
+
+});
